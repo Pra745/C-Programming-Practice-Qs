4a1159ccad856bf017302880eaaafd6da2175c46
Prayanshi <155210868+Pra745@users.noreply.github.com>
2025-09-13 22:42:09 +0530

    Update Day of the week.c

--- Switch/Day of the week.c
+++ Switch/Day of the week.c
@@ -8,25 +8,25 @@ int main()
     switch (d) 
     {
         case 1:
-            printf("Monday\n");
+            printf("Monday");
             break;
         case 2:
-            printf("Tuesday\n");
+            printf("Tuesday");
             break;
         case 3:
-            printf("Wednesday\n");
+            printf("Wednesday");
             break;
         case 4:
-            printf("Thursday\n");
+            printf("Thursday");
             break;
         case 5:
-            printf("Friday\n");
+            printf("Friday");
             break;
         case 6:
-            printf("Saturday\n");
+            printf("Saturday");
             break;
         case 7:
-            printf("Sunday\n");
+            printf("Sunday");
             break;
         default:
             printf("Invalid input. Please enter a number between 1 and 7.");
