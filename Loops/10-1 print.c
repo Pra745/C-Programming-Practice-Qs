b5aa8f4000f62fe3ed6efc48cc560424a0207786
Prayanshi <155210868+Pra745@users.noreply.github.com>
2025-09-14 23:21:38 +0530

    Create 10-1 print.c

---
+++ Loops/10-1 print.c
@@ -0,0 +1,10 @@
+//Prints the first 10 natural numbers in reverse order from 10 to 1.
+#include <stdio.h>
+
+int main() 
+{
+    int i;
+    for (i = 10; i >= 1; i--) 
+        printf("%d ", i); 
+    return 0;
+}
