c50d534e3694fd079f3bc94e9ba5e1c647c774c1
Prayanshi <155210868+Pra745@users.noreply.github.com>
2025-09-14 23:10:26 +0530

    Update and rename Perfect square to If else/Perfect square.c

--- If else/Perfect square.c
+++ If else/Perfect square.c
@@ -1,3 +1,5 @@
+//Checks if a given number is a perfect square number or not.
+//Perfect square is an integer that results from an integer being multiplied by itself, or an integer raised to the power of two
 #include <stdio.h>
 #include <math.h>
 
