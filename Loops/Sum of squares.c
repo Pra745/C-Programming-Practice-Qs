5a22a647ec0bb6f6d26861c67e702dcbb2f45a04
Prayanshi <155210868+Pra745@users.noreply.github.com>
2025-09-15 21:20:23 +0530

    Create Sum of squares.c

---
+++ Loops/Sum of squares.c
@@ -0,0 +1,13 @@
+//Prints the sum of square of N natural numbers.
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, sum;
+    printf("Enter a number");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) 
+        sum=sum+(i*i);
+    printf("Sum =", sum);
+    return 0;
+}
