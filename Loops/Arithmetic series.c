2d44b0a7be71e72bd2b60af41da04181a60e3931
Prayanshi <155210868+Pra745@users.noreply.github.com>
2025-09-15 22:31:40 +0530

    Create Arithmetic series.c

---
+++ Loops/Arithmetic series.c
@@ -0,0 +1,16 @@
+//Prints the arithmetic progression series.
+#include <stdio.h>
+int main()
+{
+    int a, n, d, c;
+    printf("Enter first term, common difference, number of terms: ");
+    scanf("%d %d %d", &a, &n, &d);
+    printf("The arithmetic progression series is: ");
+    c=a;
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", c);
+        c=c+d;
+    }
+    return 0;
+}
