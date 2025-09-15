81f83f1e72541f87c2cef7a8188e7ed6f8deb4eb
Prayanshi <155210868+Pra745@users.noreply.github.com>
2025-09-15 21:39:40 +0530

    Create Prime numbers in range.c

---
+++ Loops/Prime numbers in range.c
@@ -0,0 +1,19 @@
+//Prints all the prime number in a range.
+#include <stdio.h>
+void main()
+{
+    printf("Enter two numbers for the range");
+    int n=0, m=0;
+    scanf("%d %d",&n, &m);
+    int count=0;
+    for (int a=n; a<=m; a++)
+    {
+        for (int i=1; i<=a;i++)
+        {
+             if (a/i==2)
+             count++;
+        }
+        if (count==2)
+        printf("%d ,", &n);
+    }
+}
